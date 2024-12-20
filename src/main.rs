1103beaa6c66d3bdd32f43b02072eb235c377690
Bridget Hartin Pasco <isaacpasco@icloud.com>
2024-12-20 15:54:21 -0800

    bruh

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use raylib::{color, prelude::RaylibDraw};
 pub mod runtime;
 mod utils;
 fn main(){
-    runtime::run(&||{println!("did a thing")}, &||{}, &|handle|{handle.draw_text("hey toast i love you", 400, 500, 24, color::Color::PINK);});
+    runtime::run(&||{println!("did a thing")}, &||{println!("f")}, &|handle|{handle.draw_text("hey toast i love you", 400, 500, 24, color::Color::PINK);});
 }
